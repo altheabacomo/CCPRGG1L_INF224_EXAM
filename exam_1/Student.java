254d7865bdb5a64a4c4f12ea42bce47005f6f4cc
altheabacomo <111822135+altheabacomo@users.noreply.github.com>
2022-09-09 12:57:51 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,31 @@
+public class Student{
+
+    String firstName;
+    String surName;
+    Character middleInitial;
+    int dateOfBirth;
+    int studentNumber;
+    String EmailAddress;
+    Boolean iAmAwesome;
+    public String studentEmailAddress;
+
+    public void sayMystudentNumber(){
+    
+      System.out.println("My Student Number is"+ 2022109695);
+    
+    }
+
+    public void sayMyEmailAddress(){
+        
+      System.out.println("My Email Address is"+ "bacomoaa@students.national-u.edu.ph");
+
+    }
+
+    public void sayIamAwesome(){
+    
+      System.out.println("I am Awesome"+ iAmAwesome);
+
+    }
+    
+
+}
