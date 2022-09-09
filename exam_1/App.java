37daa992044b3d821afb10f474d506ec68af5465
altheabacomo <111822135+altheabacomo@users.noreply.github.com>
2022-09-09 12:56:57 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    Student Me = new Student();
+    Me.surName = "Bacomo";
+    Me.firstName = "Francess Althea";
+    Me.middleInitial = 'A';
+    Me.dateOfBirth = 12/29/03;
+    Me.studentNumber = 2022109695;
+    Me.studentEmailAddress = "bacomoaa@students.national-u.edu.ph";
+    Me.iAmAwesome = true;
+    Me.sayMystudentNumber();
+    Me.sayMyEmailAddress();
+    Me.sayIamAwesome();
+    }
+   }
